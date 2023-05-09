5349a48ca72e3e8a6348b06aec6b6e8ac1153e84
DeltaV <127794765+Delta-Airlines-ig@users.noreply.github.com>
2023-05-09 08:40:15 -0400

    Update StringGenerator.java

--- src/randomMindustry/random/util/StringGenerator.java
+++ src/randomMindustry/random/util/StringGenerator.java
@@ -1,27 +1,24 @@
 package randomMindustry.random.util;
 
 public class StringGenerator {
-    public static final String[] VOWELS = new String[]{
-            "i", "e", "u", "a", "ee", "ea", "ie", "ou", "er", "or", "o", "ir", "ur", "ear", "our", "ar",
-            "al", "au", "ae", "oar", "oo", "ui", "ew", "a", "eigh", "ow", "oa", "oi", "oy", "eer",
-            "ere", "are", "air", "oor"
+    public static final String[] ELEMENTS = new String[]{
+            "hydrogen", "oxygen", "carbon", "titanium", "iron", "aluminum", "sulfur", "clorine", "lead", "nitrogen"
     };
-    public static final String[] CONSONANTS = new String[]{
-            "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x",
-            "y", "z"
+    public static final String[] NUMBERS = new String[]{
+            " mono", " di", " tri", " penta", " hexa", " hepta", " octa", " nona", " deca", " tetra", " "
     };
-    public static final String[] TEMPLATE = new String[]{
-            "cv", "vc", "cvc", "vcv"
+    public static final String[] CHEM = new String[]{
+            "NEN", "N", "ENEN", "NENENENEN", "NENENE"
     };
 
     public static String generateSyllable() {
-        String template = RandomUtil.random(TEMPLATE, RandomUtil.getClientRand());
+        String sequence = RandomUtil.random(CHEM, RandomUtil.getClientRand());
         StringBuilder out = new StringBuilder();
         for (int i = 0; i < template.length(); i++) {
-            if (template.charAt(i) == 'c') {
-                out.append(RandomUtil.random(CONSONANTS, RandomUtil.getClientRand()));
+            if (sequence.charAt(i) == 'N') {
+                out.append(RandomUtil.random(NUMBERS, RandomUtil.getClientRand()));
             } else {
-                out.append(RandomUtil.random(VOWELS, RandomUtil.getClientRand()));
+                out.append(RandomUtil.random(ELEMENTS, RandomUtil.getClientRand()));
             }
         }
         return out.toString();
@@ -29,7 +26,7 @@ public class StringGenerator {
 
     public static String generateWord(int size) {
         StringBuilder out = new StringBuilder();
-        for (int i = 0; i < size; i++) out.append(generateSyllable());
+        for (int i = 0; i <= 1; i++) out.append(generateSyllable());
         return out.toString();
     }
     
@@ -47,14 +44,13 @@ public class StringGenerator {
 
     public static String generateMaterialSuffix() {
         return RandomUtil.random(new String[]{
-                "ite", "ium", "ide", "ast", "ant", "ten"
+            "hydride", "oxide", "carbide", "sulfide", "cloride", "nitride"
         }, RandomUtil.getClientRand());
     }
 
     public static String generateMaterialSuffixType() {
         return RandomUtil.random(new String[]{
-                "Alloy", "Fabric", "Compound", "Matter", "Pod", "Cyst",
-                "Clust", "Crystal", "Block", "Sphere", "Spore", "Mix"
+                "Acid", "Oxide", "Carbide", "Nitrate", "Ferrite"
         }, RandomUtil.getClientRand());
     }
 
@@ -170,7 +166,7 @@ public class StringGenerator {
     public static String generateSectorPlace() {
         return RandomUtil.random(new String[]{
                 "Market", "Zero", "Facility", "Craters", "Canyons", "Floors", "Mountains",
-                "Meteor", "Ruins", "Shores", "Bunker", "Flats"
+                "Meteor", "Ruins", "Shores", "Bunker", "Flats", "Battlefield", "Impact"
         }, RandomUtil.getClientRand());
     }
 
